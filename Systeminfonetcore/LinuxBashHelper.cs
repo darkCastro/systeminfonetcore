f5be972918a5a86fd94c0de2b5717f659a73d3ff
DarkCastro <data2354@gmail.com>
2018-05-29 00:16:17 -0400

    comit 1

---
+++ Systeminfonetcore/LinuxBashHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+namespace Systeminfonetcore
+{
+ public static class LinuxBashHelper
+    {
+
+        /// <summary>
+        /// clase para enviar commandos al 
+        /// </summary>
+        /// <param name="cmd"></param>
+        /// <returns></returns>
+
+        public static string Bash(this string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+
+
+    }
+}
---
+++ Systeminfonetcore/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Systeminfonetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (OperatingSystem.IsWindows())
+            {             
+
+                Console.WriteLine("ESTOY TRABAJANDO CON WINDOWS ..!! ");
+
+                Console.Read();
+            }
+
+        }
+    }
+
+
+        public static class OperatingSystem
+        {
+            public static bool IsWindows() =>
+
+                RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+            public static bool IsMacOS() =>
+                RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+            public static bool IsLinux() =>
+                RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        }
+
+    
+}
